d43430c4020d759eb791cfbfb7120d3d8b481089
vs <93345001+ViktorijaEl@users.noreply.github.com>
2024-04-05 13:28:13 +0300

    Update javaA.js

--- javaA.js
+++ javaA.js
@@ -11,3 +11,37 @@ document.addEventListener("DOMContentLoaded", function() {
     document.body.classList.remove("loading");
   });
 });
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    let consent = getCookie("userConsent");
+    if (!consent) {
+        document.getElementById('cookieConsent').style.display = 'block';
+    }
+});
+
+function setCookiePreferences(choice) {
+    setCookie("userConsent", choice, 365); // Stores user consent for 365 days
+    // Here, implement function to handle consent. For 'mandatory', disable optional cookies.
+    document.getElementById('cookieConsent').style.display = 'none';
+}
+
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
